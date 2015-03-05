651fa60dc5aa796ff7f16f9be21e102d6b9783cd
Lewis Barrnes <lewis@lewis-pc.(none)>
2015-03-05 00:41:49 +0000

    Added support for closing on click and edge slide in options.

--- index.js
+++ index.js
@@ -61,6 +61,8 @@ function Slideout(options) {
   this._duration = parseInt(options.duration, 10) || 300;
   this._tolerance = parseInt(options.tolerance, 10) || 70;
   this._padding = parseInt(options.padding, 10) || 256;
+  this._closeOnClick = options.closeOnClick || false;
+  this._grabWidth = parseInt(options.grabWidth, 10) || 0;
 
   // Init touch events
   this._initTouchEvents();
@@ -154,14 +156,25 @@ Slideout.prototype._initTouchEvents = function() {
     }
   });
 
+  if (this._closeOnClick) {
+    /**
+     * Close menu when panel is clicked while open
+     */
+    this.panel.addEventListener('click', function() {
+      if (self.isOpen) { self.close(); }
+    });
+  }
+
   /**
    * Resets values on touchstart
    */
   this.panel.addEventListener(touch.start, function(eve) {
     self._moved = false;
     self._opening = false;
-    self._startOffsetX = eve.touches[0].pageX;
-    self._preventOpen = (!self.isOpen() && self.menu.clientWidth !== 0);
+            
+    var offset = eve.touches[0].pageX;
+    self._startOffsetX = offset;
+    self._preventOpen = (!self.isOpen() && (self.menu.clientWidth !== 0 || (self._grabWidth && offset > self._grabWidth)));
   });
 
   /**
